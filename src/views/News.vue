c3ebaad8bccbc4a0a60d4d0275104774c25d02f8
hummingcattery <159630220+hummingcattery@users.noreply.github.com>
2024-04-29 21:33:02 +0300

    Update News.vue

--- src/views/News.vue
+++ src/views/News.vue
@@ -4,6 +4,18 @@
     </div>
 
     <div class="content-container">
+        <p><strong>29.4.2024</strong></p>
+        <p>
+        Pennut täyttivät tänään 9 viikkoa, vauhtia ja vaarallisia tilanteita riittää. Pentujen ensimmäinen rokotus on varattu 20.5. päivälle, jolloin pennut saavat myös mikrosirut.<br>
+        Nykyiset rokotussuositukset ovat sellaiset, että ensimmäinen rokote suositellaan annettavan 12 viikon iässä, ja toinen tästä 3-4 viikon päästä. Täten meillä pennut tulevat olemaan <br>
+        luovutuskypsiä vasta 16-viikkoisina, täydellä rokotussuojalla. Pennuista on tullut jonkin verran kyselyjä (lähinnä lemmikkikoteihin), mutta nämä pennut etsivät edelleen ensisijassa sijoituskoteja. <br>
+        Tärkeintä on, että tästä pentueesta molemmille tytöille tulee mahdollisuus suvun jatkolle. Otathan rohkeasti yhteyttä, jos sijoituskotina toimiminen yhtään kiinnostaa! :) 
+        </p>
+    </div>
+    
+    <hr>
+
+    <div class="content-container">
         <p><strong>14.4.2024</strong></p>
         <p>
         Hillevin ja Copan pennut syntyivät 26.2.2024. Kaksi ruskeanaamio-tyttöpentua, jotka etsivät molemmat sijoituskotia.<br>
