788df992f56c1387499d722748166453e08319c7
Tomi <ot_tomi@hotmail.com>
2024-10-24 22:37:25 +0300

    Add Vilma to Mollies page

--- src/views/Mollies.vue
+++ src/views/Mollies.vue
@@ -1,12 +1,14 @@
 <script>
     import HilleviImg from '../assets/images/Hillevi_01.jpg'
     import HuldaImg from '../assets/images/Hulda_01.jpg'
+    import VilmaImg from '../assets/images/Vilma_02.jpg'
     
     export default {
         data() {
             return {
                 hilleviImg: HilleviImg,
-                huldaImg: HuldaImg
+                huldaImg: HuldaImg,
+                vilmaImg: VilmaImg
             };
         }
     };
@@ -15,13 +17,28 @@
 <template>
     <div>
         <div class="content-container">
-            <h5>"Hillevi" Demi Johnnatarie -E</h5>
+            <h5>"Vilma" KCH Fi*Humming California Girls</h5>
+            <p>SBI n
+                <br>
+                s. 26.2.2024
+            </p>
+            <p>Näyttelymenestys: 3xNOM, 3xBIS</p>
+            <a href="https://kissat.kissaliitto.fi/Pedigree?id=329981" target="_blank">Sukutaulu</a>
+            <div>
+                <img class="img-fluid" title="Fi*Humming California Girls" :src="vilmaImg" alt="Vilma" width="1300" height="953">
+            </div>
+        </div>
+
+        <hr>
+
+        <div class="content-container">
+            <h5>"Hillevi" CH Demi Johnnatarie -E</h5>
             <p>SBI b
                 <br>
                 s. 1.3.2022
             </p>
             <p>Yhteisomistuksessa LeeviL-kissalan kanssa</p>
-            <p>Näyttelymenestys (nuorten luokka): 3xBIV, 5xNOM, 1xBIS</p>
+            <p>Näyttelymenestys: 4xBIV, 7xNOM, 3xBIS</p>
             <a href="https://kissat.kissaliitto.fi/Pedigree?id=296517" target="_blank">Sukutaulu</a>
             <div>
                 <img class="img-fluid" title="Demi Johnnatarie -E" :src="hilleviImg" alt="Hillevi" width="1300" height="953">
