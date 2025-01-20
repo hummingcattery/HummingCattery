85230f87605947e3fa2d1b4b6423c837ddb45817
Tomi <ot_tomi@hotmail.com>
2025-01-20 18:50:02 +0200

    Convert Footer to CompositionAPI

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -3,41 +3,45 @@
     import InstagramIcon from '../components/icons/Instagram_icon.svg'
     
     export default {
-        data() {
+        setup() {
+            const facebookIcon = FacebookIcon;
+            const instagramIcon = InstagramIcon;
+
             return {
-                facebookIcon: FacebookIcon,
-                instagramIcon: InstagramIcon
-            };
+                facebookIcon,
+                instagramIcon,
+            }
         }
     };
 </script>
 
 <template>
     <br>
+
     <div class="footer">
         <div class="footer-content">
             <footer class="text-center text-white" style="background-color: #F8F3FC;">
                 <div class="container pt-4">
                     <section>
-                    <!-- Facebook -->
-                    <a
-                        class="btn btn-link btn-floating btn-lg text-dark m-1"
-                        href="https://www.facebook.com/hummingcattery/"
-                        role="button"
-                        data-mdb-ripple-color="dark"
-                        target="blank_">
-                        <img class="icon-image" :src="facebookIcon" alt="Facebook icon">
-                    </a>
+                        <!-- Facebook -->
+                        <a
+                            class="btn btn-link btn-floating btn-lg text-dark m-1"
+                            href="https://www.facebook.com/hummingcattery/"
+                            role="button"
+                            data-mdb-ripple-color="dark"
+                            target="blank_">
+                            <img class="icon-image" :src="facebookIcon" alt="Facebook icon">
+                        </a>
 
-                    <!-- Instagram -->
-                    <a
-                        class="btn btn-link btn-floating btn-lg text-dark m-1"
-                        href="https://www.instagram.com/hummingcattery/"
-                        role="button"
-                        data-mdb-ripple-color="dark"
-                        target="blank_">
-                        <img class="icon-image" :src="instagramIcon" alt="Instagram icon">
-                    </a>
+                        <!-- Instagram -->
+                        <a
+                            class="btn btn-link btn-floating btn-lg text-dark m-1"
+                            href="https://www.instagram.com/hummingcattery/"
+                            role="button"
+                            data-mdb-ripple-color="dark"
+                            target="blank_">
+                            <img class="icon-image" :src="instagramIcon" alt="Instagram icon">
+                        </a>
                     </section>
                 </div>
                 <div>
