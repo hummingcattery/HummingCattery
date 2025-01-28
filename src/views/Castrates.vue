912ab07bbdd5244cdc29c49fa0d51d5a118751d3
Tomi <ot_tomi@hotmail.com>
2025-01-28 15:47:28 +0200

    Clean unnecessary code

--- src/views/Castrates.vue
+++ src/views/Castrates.vue
@@ -18,6 +18,3 @@
         />
   </div>
 </template>
-
-<style lang="scss" scoped>
-</style>
--- src/views/InMemoriam.vue
+++ src/views/InMemoriam.vue
@@ -30,23 +30,5 @@
             :exhibitionSuccessResults="['Multi BIS, BIV, NOM -winner']"
             :image="harppuImg"
         />
-        <!-- <div class="content-container">
-            <h5>"Harppu" SP, SC Zhamanen Harlequin, DVM DSM</h5>
-            <p>SBI n 21
-                <br>
-                28.7.2008 - 12.12.2023
-            </p>
-            <p>
-                isä: EC FI*JoJon El Rayo Rumba (SBI n 21)
-                <br>
-                emo: IC FI*Zhamanen Ferrarina N.F.S. (SBI h)
-            </p>
-            <p>Näyttelymenestys:
-                <span>Multi BIS, BIV, NOM -winner</span>
-            </p>
-            <div>
-                <img class="img-fluid" title="SC FI*Zhamanen Harlequin, DVM" :src="harppuImg" alt="Harppu" width="900" height="660">
-            </div>
-        </div> -->
   </div>
 </template>
