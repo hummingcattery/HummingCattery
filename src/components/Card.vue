374be71d66777bc6ddf0192100e1da79a6fdff5c
Tomi <ot_tomi@hotmail.com>
2025-01-28 15:43:17 +0200

    Add a Card component

---
+++ src/components/Card.vue
@@ -0,0 +1,59 @@
+<script setup>
+    import {defineProps} from "vue";
+
+    const props = defineProps({
+        name: String,
+        variety: String,
+        lifeDates: String,
+        tom: String,
+        queen: String,
+        exhibitionSuccessResults: Array,
+        image: String
+    });
+</script>
+
+<template>
+    <div class="content-container">
+        <h5>{{props.name}}</h5>
+        <p>{{props.variety}}
+            <br>
+                s. {{props.lifeDates}}
+            </p>
+        <p>
+            isä: {{props.tom}}
+            <br>
+            emo: {{props.queen}}
+            </p>
+        <p>Näyttelymenestys:
+            <span>{{props.exhibitionSuccessResults[0]}}</span>,
+            <span>{{props.exhibitionSuccessResults[1]}}</span>
+        </p>
+        <div col-3>
+            <img class="img-fluid" :title="props.name" :src="props.image" :alt="props.name" width="900" height="600">
+        </div>
+    </div>
+</template>
+    
+<style lang="scss" scoped>
+    h5 {
+        font-weight: bold;
+    }
+
+    img {
+        object-fit: contain;
+        transform: scale(0.75);
+        display: block;
+        margin-left: auto;
+        margin-right: auto;
+    }
+    
+    .content-container {
+        background-color: #F8F3FC;
+        display: flex;
+        flex-wrap: nowrap;
+        flex-direction: column;
+        justify-content: space-evenly;
+        align-items: flex-start;
+        padding: 2rem;
+    }
+</style>
