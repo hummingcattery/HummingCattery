18a4816ada160ecfc59bb73ffbee0cefbd144638
Tomi <ot_tomi@hotmail.com>
2025-01-28 17:20:41 +0200

    Create a CardWithThumbnails component

---
+++ src/components/CardWithThumbnails.vue
@@ -0,0 +1,70 @@
+<script setup lang="ts">
+    import {defineProps} from "vue";
+    import Thumbnails from "./Thumbnails.vue";
+
+    const props = defineProps<{
+        name: string;
+        variety: string;
+        lifeDates: string;
+        comments?: string; // Optional prop
+        exhibitionSuccessResults: string[]; // Specify array type
+        pedigreeAddress: string;
+        image: string;
+        gallery: string[]; // Specify array type
+        modalOpening: () => void; // Function type
+    }>();
+</script>
+
+<template>
+    <div class="content-container">
+        <h5>{{props.name}}</h5>
+        <p>{{props.variety}}<br />s. {{props.lifeDates}}</p>
+        <p v-if="props.comments">{{props.comments}}</p>
+        <p>Näyttelymenestys: {{props.exhibitionSuccessResults[0]}}</p>
+        <a
+            :href="props.pedigreeAddress"
+            target="_blank"
+            >Sukutaulu</a
+        >
+        <div class="img-container">
+            <img
+                class="img-fluid scaledImg"
+                :title="props.name"
+                :src="props.image"
+                alt="Vilma"
+                width="1300"
+                height="953"
+            />
+            <Thumbnails :gallery="props.gallery" :modalOpening="props.modalOpening" />
+        </div>
+    </div>
+</template>
+    
+<style lang="scss" scoped>
+    h5 {
+        font-weight: bold;
+    }
+    
+    .content-container {
+        background-color: #F8F3FC;
+        display: flex;
+        flex-wrap: nowrap;
+        flex-direction: column;
+        justify-content: space-evenly;
+        align-items: flex-start;
+        padding: 2rem;
+    }
+
+    .img-container {
+        margin-left: auto;
+        margin-right: auto;
+    }
+
+    .scaledImg {
+        object-fit: contain;
+        transform: scale(0.75);
+        display: block;
+        margin-left: auto;
+        margin-right: auto;
+    }
+</style>
