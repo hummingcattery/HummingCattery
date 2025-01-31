9e3e0ffd042a5e1d131f30cbf4012d735463dcde
Tomi <ot_tomi@hotmail.com>
2025-01-31 10:55:53 +0200

    Fix Google Analytics

--- src/main.js
+++ src/main.js
@@ -10,4 +10,34 @@ import "bootstrap/dist/js/bootstrap.js";
 
 const pinia = createPinia();
 
+// Load Google Tag dynamically
+const GA_ID = import.meta.env.VITE_GOOGLE_ANALYTICS;
+
+if (GA_ID) {
+    const script1 = document.createElement("script");
+    script1.async = true;
+    script1.src = `https://www.googletagmanager.com/gtag/js?id=${GA_ID}`;
+    document.head.appendChild(script1);
+
+    const script2 = document.createElement("script");
+    script2.innerHTML = `
+        window.dataLayer = window.dataLayer || [];
+        function gtag(){dataLayer.push(arguments);}
+        gtag('js', new Date());
+        gtag('config', '${GA_ID}');
+    `;
+    document.head.appendChild(script2);
+
+    console.log("script2 = ", script2)
+}
+
+// Track page views when route changes
+router.afterEach((to) => {
+    if (window.gtag) {
+        window.gtag("config", GA_ID, {
+        page_path: to.fullPath,
+        });
+    }
+});
+
 createApp(App).use(router).use(pinia).mount("#app");
