9c6c1c5f9a7a2603e00b00c6337ec552b9425220
Tomi <ot_tomi@hotmail.com>
2025-02-02 15:19:33 +0200

    Update date in the Footer

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -36,7 +36,7 @@
                     </section>
                 </div>
                 <div>
-                    <p>Päivitetty 31.1.2025</p>
+                    <p>Päivitetty 2.2.2025</p>
                 </div>
             </footer>
         </div>
