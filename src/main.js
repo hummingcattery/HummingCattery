a69aec13221da5a6e3e68d4cc9efc6f6d0f448a0
Tomi <ot_tomi@hotmail.com>
2025-05-03 15:28:46 +0300

    Update main.js

--- src/main.js
+++ src/main.js
@@ -7,6 +7,7 @@ import App from './App.vue'
 import router from './router'
 import "bootstrap/dist/css/bootstrap.css";
 import "bootstrap/dist/js/bootstrap.js";
+import VueGtag from "vue-gtag-next";
 
 const app = createApp(App);
 const pinia = createPinia();
@@ -14,7 +15,7 @@ const pinia = createPinia();
 app.use(pinia);
 app.use(router);
 app.use(VueGtag, {
-  config: { id: import.meta.env.ITE_GOOGLE_ANALYTICS }
+    config: { id: import.meta.env.VITE_GOOGLE_ANALYTICS }
 }, router);
 
 app.mount("#app");
