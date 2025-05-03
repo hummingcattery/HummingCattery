099d71f2a55ef31ca9f0961a583659db588b5de7
Tomi <ot_tomi@hotmail.com>
2025-05-03 12:29:40 +0300

    Add analytics helper

---
+++ src/analytics.ts
@@ -0,0 +1,17 @@
+export function loadGoogleAnalytics(gaId: string) {
+    if (!gaId) return;
+  
+    const script1 = document.createElement('script');
+    script1.async = true;
+    script1.src = `https://www.googletagmanager.com/gtag/js?id=${gaId}`;
+    document.head.appendChild(script1);
+  
+    const script2 = document.createElement('script');
+    script2.innerHTML = `
+      window.dataLayer = window.dataLayer || [];
+      function gtag(){dataLayer.push(arguments);}
+      gtag('js', new Date());
+      gtag('config', '${gaId}');
+    `;
+    document.head.appendChild(script2);
+  }
