c83658582dae45e0f77caff45ebd7dac746b51ad
Tomi <ot_tomi@hotmail.com>
2025-08-27 15:57:05 +0300

    Update previously updated date

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -36,7 +36,7 @@
                     </section>
                 </div>
                 <div>
-                    <p>Päivitetty 21.7.2025</p>
+                    <p>Päivitetty 27.8.2025</p>
                 </div>
             </footer>
         </div>
