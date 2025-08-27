ad1ea831774255b4697bdd0f8d1dc2a32cf55c78
Tomi <ot_tomi@hotmail.com>
2025-08-27 15:55:27 +0300

    Add styly class for smaller pictures

--- src/main.js
+++ src/main.js
@@ -7,6 +7,7 @@ import App from './App.vue'
 import router from './router'
 import "bootstrap/dist/css/bootstrap.css";
 import "bootstrap/dist/js/bootstrap.js";
+import "./assets/main.css";
 
 const app = createApp(App);
 const pinia = createPinia();
